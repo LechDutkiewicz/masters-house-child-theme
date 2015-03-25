e0f4784f0536a0945699c8c6d58cdfe370e043cc
LechDutkiewicz <lechdutkiewicz@gmail.com>
2015-03-25 17:52:20 +0100

    added promotion block section

---
+++ templates/blocks/block-promotion-home.php
@@ -0,0 +1,53 @@
+<?php
+
+if ( !defined( 'ABSPATH' ) )
+	exit( 'No direct script access allowed' ); // Exit if accessed directly
+
+/**
+ * Shandora layout block
+ * This block handles promotions displayed on the home page
+ *
+ *
+ * @author		Lech Dutkiewicz
+ * @copyright	Copyright (c) Lech Dutkiewicz
+ * @link		http://techsavvymarketers.pl
+ * @since		Version 1.1
+ * @package 	Layouts
+ * @category 	Layout
+ *
+ *
+ */
+
+$args = array(
+	'post_type' => 'promotions',
+	'posts_per_page' => 1,
+	'orderby' => 'menu_order'
+);
+
+$loop = new WP_Query( $args );
+
+while ( $loop->have_posts() ) : $loop->the_post();
+	?>
+
+<section>
+	<header class="section-header">
+		<h3 class="home-section-header"><?php the_title(); ?></h3>
+	</header>
+	<div class="row entry-row">
+		<div class="column large-12">
+			<?php if ( current_theme_supports( 'get-the-image' ) )
+		get_the_image( array( 'size' => 'full', 'image_class' => 'auto' ) ); ?>
+		</div>
+		<!--<div class="column large-4">
+			<p><?php the_content('', TRUE); ?></p>
+			<a href="<?php the_permalink(); ?>" class="flat button wet-asphalt radius cta"><?php _e( 'Read more', 'bon' ); ?></a>
+		</div>-->
+		<!--<a href="<?php the_permalink(); ?>" class="flat button wet-asphalt radius cta cta-absolute"><?php _e( 'Read more', 'bon' ); ?></a>-->
+	</div>
+</section>
+
+	<?php
+endwhile;
+wp_reset_query();
+
+?>
