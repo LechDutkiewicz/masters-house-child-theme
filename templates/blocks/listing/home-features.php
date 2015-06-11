2a615fb07c18d30022df00e16f30898ba081813e
LechDutkiewicz <lechdutkiewicz@gmail.com>
2015-06-11 15:35:56 +0200

    updated layout for icons of services

--- templates/blocks/listing/home-features.php
+++ templates/blocks/listing/home-features.php
@@ -5,34 +5,38 @@ $args = array(
 	'posts_per_page' => 10,
 	'orderby' => 'menu_order',
 	'order' => 'ASC'
-);
+	);
 $loop = new WP_Query( $args );
 if ( !empty( $loop->posts ) ) :
 	?>
-	<section>
-		<?php if (shandora_is_home()) : ?>
-		<header class="section-header">
-			<h3 class="home-section-header"><?php echo bon_get_option( 'home_features_title', 'yes' ); ?></h3>
-		</header>
-		<?php else : ?>
-		<header>
-			<h3><?php echo bon_get_option( 'home_features_title', 'yes' ); ?></h3>
-			<hr />
-		</header>
-		<?php endif; ?>
-		<div  class="row entry-row">
-			<div class="padding-medium clearfix">
-				<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-					<div class="column large-4 text text-center service-container <?php echo $exClass; ?>">
-						<i class="text bonicons <?php echo shandora_get_meta( $post->ID, 'featureicon' ) . ' ' . shandora_get_meta( $post->ID, 'featureiconcolor' ); ?>"></i>
-						<h5 class="service-title"><?php the_title(); ?></h5>
-						<p class="entry-content"><?php echo get_the_content(); ?></p>
-					</div>
-					<?php
-				endwhile;
-			endif;
-			wp_reset_query();
-			?>
+<section>
+	<?php if (shandora_is_home()) : ?>
+	<header class="section-header">
+		<h3 class="home-section-header"><?php echo bon_get_option( 'home_features_title', 'yes' ); ?></h3>
+	</header>
+<?php else : ?>
+	<header>
+		<h3><?php echo bon_get_option( 'home_features_title', 'yes' ); ?></h3>
+		<hr />
+	</header>
+<?php endif; ?>
+<div  class="row entry-row">
+	<div class="padding-medium clearfix">
+		<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+		<div class="column large-4 text service-container <?php echo $exClass; ?>">
+			<div class="round-icon icon-wrapper bg-flat-<?php echo shandora_get_meta( $post->ID, 'featureiconcolor' ); ?>">
+				<i class="icon-anim-left-right bonicons <?php echo shandora_get_meta( $post->ID, 'featureicon' ); ?>"></i>
+			</div>
+			<div class="service-content">
+				<h2 class="service-title"><?php the_title(); ?></h2>
+				<p class="service-summary"><?php echo get_the_content(); ?></p>
+			</div>
 		</div>
+		<?php
+		endwhile;
+		endif;
+		wp_reset_query();
+		?>
 	</div>
+</div>
 </section>
--- templates/blocks/listing/services.php
+++ templates/blocks/listing/services.php
@@ -1,7 +1,7 @@
 <?php if ( $_SESSION['layoutType'] === 'mobile' ) $exClass = 'text-center'; ?>
 <?php if ( is_singular( 'listing' ) ) : ?>
 	<header>
-		<h3 class="services-header"><?php _e( 'Services', 'bon' ); ?></h3>
+		<h3 class="services-header"><?php _e( 'With us you will', 'bon' ); ?></h3>
 		<hr />
 	</header>
 <?php else: ?>
@@ -12,22 +12,64 @@ $args = array(
 	'posts_per_page' => 10,
 	'orderby' => 'menu_order',
 	'order' => 'ASC'
-);
+	);
 $loop = new WP_Query( $args );
 if ( !empty( $loop->posts ) ) :
 	?>
-	<div class="row">
-		<?php while ( $loop->have_posts() ) : $loop->the_post();
-			?>
-			<div class="column large-12 service-container <?php echo $exClass; ?>">
-				<i class="text bonicons <?php echo shandora_get_meta( $post->ID, 'serviceicon' ) . ' ' . shandora_get_meta( $post->ID, 'serviceiconcolor' ); ?>"></i>
-				<h5 class="service-title"><?php the_title(); ?></h5>
-				<p class="entry-content"><?php echo get_the_content(); ?></p>
-			</div>
-		<?php endwhile;
-		?>
+<div class="row">
+	<?php while ( $loop->have_posts() ) : $loop->the_post();
+
+	// setup variables
+	$more_title = shandora_get_meta( $post->ID, 'service_more_title' );
+	$more_content = shandora_get_meta( $post->ID, 'service_more_content', false, false );
+	$sanitazed_title = sanitize_title( $more_title );
+
+	?>
+
+	<div class="column large-12 service-container <?php echo $exClass; ?>">
+		<div class="round-icon icon-wrapper bg-flat-<?php echo shandora_get_meta( $post->ID, 'serviceiconcolor' ); ?>">
+			<i class="icon-anim-left-right text bonicons <?php echo shandora_get_meta( $post->ID, 'serviceicon' ); ?>"></i>
+		</div>
+		<div class="service-content">
+			<h3 class="service-title"><?php the_title(); ?></h2>
+			<p class="entry-content"><?php echo get_the_content(); ?>
+
+				<?php // create read more button to open modal with extended information if it's available ?>
+				<?php if ( $more_title && $more_content ) { ?>
+
+				<a href="#<?php echo sanitize_title( $more_title ); ?>" role="button" data-toggle="modal" class="button flat main radius tiny" title="">
+					<span class="cta-headline"><?php _e( 'Read more', 'bon'); ?></span>
+				</a>
+
+				<?php } ?>
+
+			</p>
+		</div>
 	</div>
-	<?php
+
+	<?php // create modal with extended information if it's available ?>
+	<?php if ( $more_title && $more_content ) { ?>
+
+	<div id="<?php echo $sanitazed_title; ?>" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="<?php echo $sanitazed_title; ?>-modal-label" aria-hidden="true">
+		<div class="modal-dialog modal-lg">
+			<div class="modal-content">
+				<div class="modal-header bg <?php echo bon_get_option( 'search_button_color', 'red' ); ?>">
+					<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+					<h4 class="modal-title" id="<?php echo $sanitazed_title; ?>-modal-label"><?php echo $more_title ?></h4>
+				</div>
+				<div class="modal-body">
+					<?php shandora_sanitize_content($more_content); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<?php } ?>
+
+	<?php endwhile;
+	?>
+</div>
+<?php
 endif;
 wp_reset_query();
 
