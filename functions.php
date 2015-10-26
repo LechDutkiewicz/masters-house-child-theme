62e281f41bae53e4e017cf3d1c7c7a5e3cd19cec
LechDutkiewicz <lechdutkiewicz@gmail.com>
2015-10-26 14:28:48 +0100

    added AdWords conversion tag and fixed couple mobile display errors

--- functions.php
+++ functions.php
@@ -49,5 +49,152 @@ add_filter('shandora_dynamic_style', 'my_child_theme_remove_single_parent_style'
 */
 /* Reload your page and WHOAAA!! superr!!! :) */
 
+function shandora_print_phone_call_tracking_code() {
+
+	if ( WP_ENV === 'production' && !is_user_logged_in() ) {
+
+		$scripts = bon_get_option('google_adwords');
+
+		if(!empty($scripts)) { ?>
+
+		<!-- Google AdWords call tracking code -->
+		<?php echo $scripts; ?>
+
+
+		<?php 
+	}
+
+}
+
+}
+
+add_action('wp_head', 'shandora_print_phone_call_tracking_code', 103);
+
+function shandora_get_main_header() {
+	$header_style = bon_get_option( 'main_header_style', 'dark' );
+	$state = bon_get_option( 'show_main_header', 'show' );
+	$header_col_1 = bon_get_option( 'enable_header_col_1' );
+	$header_col_2 = bon_get_option( 'enable_header_col_2' );
+	$center_logo = bon_get_option( 'centering_logo' );
+
+	$header_col_class = 'large-9';
+	$col_class = 'large-6';
+	$logo_class = 'uncentered';
+	$logo_col_class = 'large-3';
+	if ( $header_col_1 == true && $header_col_2 == true ) {
+		$header_col_class = 'large-9 small-12';
+		$col_class = 'large-6 small-12';
+	} else if ( $header_col_1 == true && $header_col_2 == false ) {
+		$header_col_class = 'large-5 small-12';
+		$col_class = 'large-12';
+	} else if ( $header_col_1 == false && $header_col_2 == true ) {
+		$header_col_class = 'large-5 small-12';
+		$col_class = 'large-12';
+	} else {
+
+		$logo_class = 'full';
+		$logo_col_class = 'large-12';
+		if ( $center_logo == true ) {
+			$logo_class = 'centered';
+		}
+	}
+	?>
+	<hgroup id="main-header" class="<?php echo $header_style; ?> slide <?php echo $state; ?>">
+		<div class="row">
+			<?php $is_text = ((bon_get_option( 'logo' ) != '') ? false : true); ?>
+			<div class="<?php echo $logo_col_class; ?> column small-centered large-<?php echo $logo_class; ?> <?php echo ($is_text) ? 'text-logo' : ''; ?>" id="logo">
+				<div id="nav-toggle" class="navbar-handle show-for-small"></div>
+				<?php
+				$tag = 'h1';
+				if ( is_singular() && !is_home() && !is_front_page() ) {
+					$tag = 'h3';
+				}
+				?>
+				<<?php echo $tag; ?> itemprop="name" class="<?php echo $logo_class; ?>"><a href="<?php echo home_url(); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><?php if ( bon_get_option( 'logo' ) ) { ?><img itemprop="image" src="<?php echo bon_get_option( 'logo', get_template_directory_uri() . '/assets/images/logo.png' ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"/><?php
+			} else {
+				echo esc_attr( get_bloginfo( 'name', 'display' ) );
+			}
+			?></a></<?php echo $tag; ?>>
+			<?php if ( $is_text ) { ?>
+			<span class="site-description <?php echo $logo_class; ?> hide-for-desktop hide-for-small"><?php echo get_bloginfo( 'description', 'display' ); ?></span>
+			<?php } ?>
+		</div>
+
+		<?php if ( $header_col_1 == true || $header_col_2 == true ) : ?>
+		<div class="<?php echo $header_col_class; ?> column" id="company-info">
+			<div class="row">
+				<?php if ( $header_col_1 ) : ?>
+				<div class="<?php echo $col_class; ?> column small-text-center small-margin medium bottom">
+					<div class="icon hide-for-small">
+						<span class="sha-phone"></span>
+					</div>
+					<span class="info-title"><?php echo esc_attr( bon_get_option( 'hgroup1_title' ) ); ?></span>
+					<?php
+					$phone_html = '';
+					$phone = explode( ',', esc_attr( bon_get_option( 'hgroup1_content' ) ) );
+					$phone_count = count( $phone );
+					if ( $phone_count > 1 ) {
+						foreach ( $phone as $number ) {
+							$phone_html .= '<strong>' . $number . '</strong>';
+						}
+					} else {
+						$phone_html = '<strong>';
+
+						// make phone tag a clickable link for mobile devices that will fire a Google Analytics event
+						if ( $_SESSION['layoutType'] == 'mobile' ) {
+							$phone_html .= '<a href="tel:' . esc_attr( bon_get_option( 'hgroup1_content' ) ) . '"' . get_ga_event( "Contact", "Call", "Menu Bar" ) . '>';
+						}
+
+						$phone_html .= "<span class='phone phone-$phone_count'>";
+						$phone_html .= esc_attr( bon_get_option( 'hgroup1_content' ) );
+						$phone_html .= "</span>";
+
+						if ( $_SESSION['layoutType'] == 'mobile' ) {
+							$phone_html .='</a>';
+						}
+
+						$phone_html .='</strong>';
+					}
+					?>
+					<?php echo $phone_html; ?>
+				</div>
+			<?php endif; ?>
+			<?php if ( $header_col_2 ) : ?>
+			<div class="<?php echo $col_class; ?> column hide-for-small small-text-center">
+				<div class="icon">
+					<span class="sha-calendar"></span>
+				</div>
+				<span class="info-title"><?php echo bon_get_option( 'hgroup2_title' ); ?></span>
+				<span class="phone visit"><strong><a href='#visit-modal' role='button' data-toggle='modal' title="<?php echo esc_attr( bon_get_option( 'hgroup2_line1' ) ); ?>" <?php the_ga_event( "Contact", "Open Visit Request", "Menu Bar" ); ?>><?php echo esc_attr( bon_get_option( 'hgroup2_line1' ) ); ?></a></strong></span>
+			</div>
+		<?php endif; ?>
+	</div>
+</div>
+<?php endif; ?>
+
+</div>
+</hgroup> 
+<?php
+}
+
+function get_formatted_phone_number() {
+
+	$output = '<strong>';
+
+	if ( $_SESSION['layoutType'] == 'mobile' ) {
+		$output .= '<a href="tel:' . esc_attr( bon_get_option( 'hgroup1_content' ) ) . '"' . get_ga_event( "Contact", "Call", "Menu Bar" ) . '>';
+	}
+
+	$output .= esc_attr( bon_get_option( 'hgroup1_content' ) );
+
+	if ( $_SESSION['layoutType'] == 'mobile' ) {
+		$output .='</a>';
+	}
+
+	$output .='</strong>';
+
+	return $output;
+}
+
 
 ?>
---
+++ header.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Shandora Head Hook
+ *
+ * @hooke shandora_document_info - 1
+ *
+ */
+	do_atomic('head');
+
+	$class = '';
+
+	if( is_singular('product') ) {
+		$class = 'singular-listing';
+	}
+	
+	// Any mobile device (phones or tablets).
+
+?>
+<body data-spy="scroll" data-target=".affix-nav-container"  id="totop" <?php body_class( $class ); ?><?php if ( WP_ENV === 'production' && !is_user_logged_in() && bon_get_option('google_adwords') ) { ?> onload="_googWcmGet('phone', '<?php echo esc_attr( bon_get_option( 'hgroup1_content' ) ); ?>')"<?php } ?>>
+	<a href="#main-content" class="sr-only sr-only-focusable"><?php _e( 'Skip to main content', 'bon'); ?></a>
+	<div id="outer-wrap">
+
+	<!-- BEGIN Header -->
+
+	<?php
+	/**
+	 * Shandora Before Header Hook
+	 *
+	 *
+	 */
+
+	do_atomic('before_header'); ?>
+
+	<header id="header-container" class="container full">
+
+		<div id="header-inner-container" class="container" itemtype="http://schema.org/RealEstateAgent">
+
+
+			<?php 
+			/**
+			 * Shandora Header Inner Container Action Hook
+			 *
+			 * @hooked shandora_close_main_content - 1
+			 *
+			 */
+
+			do_atomic('header_content'); ?>
+			
+			
+		</div>
+
+	</header>
+	
+
+	<?php
+	/**
+	 * Shandora After Header Hook
+	 * 
+	 * @hooked shandora_get_custom_header - 1
+	 *
+	 */
+
+	do_atomic('after_header'); ?>
+	
+	<!-- END Header -->
