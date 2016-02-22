be95efb8e54b5f41c6ec9b0d0b78fb75afa095a9
LechDutkiewicz <lechdutkiewicz@gmail.com>
2016-02-22 15:37:29 +0100

    updated single product meta description icons

--- templates/blocks/listing/meta.php
+++ templates/blocks/listing/meta.php
@@ -1,106 +1,47 @@
-<?php if ( shandora_get_meta( get_the_ID(), 'listing_enable_packages' ) ) { ?>
+<?php $post_meta_icons = get_related_meta_items(); ?>
 
-<ul class="large-custom-grid-5 small-custom-grid-1">
+<ul class="mobile-custom-grid-2 xsmall-custom-grid-3 small-custom-grid-6" data-match-height>
 
-	<?php
+	<?php foreach ( $post_meta_icons as $meta_icon ) { ?>
 
-// setup variables
-
-	$sizemeasurement = bon_get_option( 'measurement' );
-	$heightmeasurement = bon_get_option( 'height_measure' );
-	$packages = get_packages_list();
-	$rooms = shandora_get_meta( $post->ID, 'listing_rooms' );
-	$size = shandora_get_meta( $post->ID, 'listing_lotsize' );
-	$packages = get_packages_list();
-	$wall = $packages[0];
-
-	?>
-
-	<?php if( !empty( $rooms ) ) { ?>
-
-	<li class="room"><div class="meta-wrap">
-		<i class="sha-room"></i>
-		<span class="meta-value">
-			<?php 
-			($rooms > 0) ? printf(_n( '1 Room', '%s Rooms', $rooms, 'bon' ), $rooms) : _e('No Room','bon'); 
-			?>
-		</span></div>
+	<li>
+		<div class="meta-wrap bg-gradient-<?php echo $meta_icon['color']; ?><?php echo bon_get_option( sanitize_title( $meta_icon['name'] ) . '_desc' ) ? ' has-descr' : ''; ?>" data-height-watch>
+			<figure>
+				<img src="<?php echo trailingslashit( BON_THEME_URI ); ?>assets/images/meta-icons/<?php echo sanitize_title( $meta_icon['name'] ); ?>.png" class="meta-img" alt="<?php _e( "Icon", "bon" ); ?>" width="80" height="80">
+				<figcaption>
+					<span class="meta-value">
+						<?php echo render_meta_value( $meta_icon ); ?>
+					</span>				
+					<?php if ( bon_get_option( sanitize_title( $meta_icon['name'] ) . '_desc' ) ) { ?>
+					<div class="button-container">
+						<a href="#" class="button small midnight-blue flat radius" data-reveal-id="meta-modal-<?php echo sanitize_title( $meta_icon['name'] ); ?>"><?php _e( "Read more", "bon" ); ?></a>
+					</div>
+					<?php } ?>
+				</figcaption>
+			</figure>
+		</div>
 	</li>
 
 	<?php } ?>
-
-	<?php if( !empty( $size ) ) { ?>
-
-	<li class="area"><div class="meta-wrap">
-		<i class="sha-area"></i>
-		<span class="meta-value">
-			<?php echo $size . ' ' . $sizemeasurement; ?>
-		</span></div>
-	</li>
-
-	<?php } ?>
-
-	<?php if( !empty( $wall ) ) { ?>
-
-	<li class="wall"><div class="meta-wrap">
-		<i class="sha-wall"></i>
-		<span class="meta-value">
-			<span><em><strong><?php _e( 'Wall thickness', 'bon' ); ?></strong></em></span>
-			<span data-meta="thickness"><?php echo($wall['package_wall_thickness']); ?></span><?php echo ' ' . strtolower($heightmeasurement); ?>
-		</span></div>
-	</li>
-
-	<?php } ?>
-
-	<?php if( !empty( $size ) ) { ?>
-
-	<li class="roof"><div class="meta-wrap">
-		<i class="sha-roof"></i>
-		<span class="meta-value">
-			<em><strong><?php _e( 'Insulated', 'bon' ); ?></strong></em> <?php _e( 'roof', 'bon' ); ?>
-		</span></div>
-	</li>
-
-	<?php } ?>
-
-	<?php if( !empty( $size ) ) { ?>
-
-	<li class="drill"><div class="meta-wrap">
-		<i class="sha-drill"></i>
-		<span class="meta-value">
-			<em><strong><?php _e( 'Assemblage', 'bon' ); ?></strong></em> <?php _e( 'included', 'bon' ); ?>
-		</span></div>
-	</li>
 
 </ul>
+
+<?php foreach ( $post_meta_icons as $meta_icon ) { ?>
+
+<?php if ( $reveal_content = bon_get_option( sanitize_title( $meta_icon['name'] ) . '_desc' ) ) { ?>
+
+<div id="meta-modal-<?php echo sanitize_title( $meta_icon['name'] ); ?>" class="reveal-modal small">
+	<div class="modal-header bg-<?php echo $meta_icon['color']; ?>">
+		<span class="modal-title like-h4 text-white">
+			<?php echo array_key_exists( 'name', $meta_icon ) ? bon_get_option( sanitize_title( $meta_icon['name'] ) . '_name' ) : null; ?>
+		</span>
+	</div>
+	<div class="modal-body">
+		<?php echo wpautop( $reveal_content ); ?>
+	</div>
+	<a class="close-reveal-modal">&#215;</a>
+</div>
 
 <?php } ?>
-
-<?php } else if ( shandora_get_meta( get_the_ID(), 'listing_enable_construction' ) ) { ?>
-
-<ul class="large-custom-grid-3 small-custom-grid-1">
-
-	<li class="timer"><div class="meta-wrap">
-		<i class="sha-timer"></i>
-		<span class="meta-value">
-			<em><strong><?php _e( 'Quick assemblage', 'bon' ); ?></strong></em>
-		</span></div>
-	</li>
-
-	<li class="roof-2"><div class="meta-wrap">
-		<i class="sha-roof-2"></i>
-		<span class="meta-value">
-			<em><strong><?php _e( 'Robust construction', 'bon' ); ?></strong></em>
-		</span></div>
-	</li>
-
-	<li class="wood"><div class="meta-wrap">
-		<i class="sha-wood"></i>
-		<span class="meta-value">
-			<em><strong><?php _e( 'Glued wood', 'bon' ); ?></strong></em>
-		</span></div>
-	</li>
-
-</ul>
 
 <?php } ?>
