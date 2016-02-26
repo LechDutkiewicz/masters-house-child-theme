2e713fd4fa996423f9c7f0e5acaccffeab113c2e
LechDutkiewicz <lechdutkiewicz@gmail.com>
2016-02-26 15:20:07 +0100

    updated html to handle hover overlays better

--- templates/blocks/block-listing-header.php
+++ templates/blocks/block-listing-header.php
@@ -12,7 +12,7 @@ $size = ( isset( $_GET['view'] ) && $_GET['view'] == 'list' ) ? 'listing_list' :
 		if ( $terms && !is_wp_error( $terms ) ) {
 			foreach ( $terms as $term ) {
 				?>
-				<a class="property-type<?php echo ($size == 'listing_list') ? ' hide-for-small' : ''; ?>" href="<?php echo get_term_link( $term->slug, "property-type" ); ?>" <?php the_ga_event('Cottage List', 'Pick category', 'Grid item'); ?>><?php echo $term->name; ?></a>
+				<a class="property-type bg-<?php echo get_cat_color($post->ID); ?><?php echo ($size == 'listing_list') ? ' hide-for-small' : ''; ?>" href="<?php echo get_term_link( $term->slug, "property-type" ); ?>" <?php the_ga_event('Cottage List', 'Pick category', 'Grid item'); ?>><?php echo $term->name; ?></a>
 				<?php
 				break; // to display only one property type
 			}
@@ -28,20 +28,21 @@ $size = ( isset( $_GET['view'] ) && $_GET['view'] == 'list' ) ? 'listing_list' :
 		}
 	}
 	?>	
-		<?php if ( current_theme_supports( 'get-the-image' ) ) { ?>
-		<a class="header-link product-link" href="<?php the_permalink(); ?>" <?php the_ga_event('Cottage List', 'Pick single cottage', 'Image'); ?>>
-			<?php
-			if ( current_theme_supports( 'get-the-image' ) ) {
-				if ( $_SESSION['layoutType'] === 'mobile' ) {
-					$size = 'mobile_tall';
-				} else {
+	<?php if ( current_theme_supports( 'get-the-image' ) ) { ?>
+	<a class="header-link product-link" href="<?php the_permalink(); ?>" <?php the_ga_event('Cottage List', 'Pick single cottage', 'Image'); ?>>
+		<div class="overlay"></div>
+		<?php
+		if ( current_theme_supports( 'get-the-image' ) ) {
+			if ( $_SESSION['layoutType'] === 'mobile' ) {
+				$size = 'mobile_tall';
+			} else {
 					// $size = 'listing_small';
-					$size = 'mobile_tall';
-				}
-				$src = get_the_image( array( 'size' => $size, 'link_to_post' => false, 'image_class' => 'auto' ) );
+				$size = 'mobile_tall';
 			}
-			?>
-		</a>
+			get_the_image( array( 'size' => $size, 'link_to_post' => false, 'image_class' => 'auto' ) );
+		}
+		?>
+	</a>
 	<?php } ?>
 
 </header><!-- .entry-header -->
