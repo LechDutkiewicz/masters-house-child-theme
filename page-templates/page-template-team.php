1790c45f08fbca56fecb744ab55246efbeeda959
LechDutkiewicz <lechdutkiewicz@gmail.com>
2016-04-26 21:00:07 +0200

    added team page template to display contacts

--- page-templates/page-template-team.php
+++ page-templates/page-template-team.php
@@ -5,6 +5,7 @@
 get_header(); 
 
 $headContact = bon_get_option( 'about_us_heading_contact' );
+$contentContact = bon_get_option( 'about_us_content_contact' );
 
 // get global agent ID to render him separately
 $agent_id = bon_get_option( 'global_agent' )[0];
@@ -33,168 +34,164 @@ $agent_id = bon_get_option( 'global_agent' )[0];
 
             <section class="row entry-content">
                <div class="column large-12">
-                    <ul class="bon-toolkit-accordion" id="accordion-main-agent">
+                <ul class="bon-toolkit-accordion" id="accordion-main-agent">
 
-                        <?php
+                    <?php
+                    // setup loop to fetch 1 most expensive item for each category to display it's image as category image
+                    $args = array(
+                        'post_type' => 'agent',
+                        'posts_per_page' => 10,
+                        'order' => 'ASC',
+                        'orderby' => 'menu_order',
+                        'post__in' => array($agent_id)
+                        );
 
-            // setup loop to fetch 1 most expensive item for each category to display it's image as category image
-                        $args = array(
-                            'post_type' => 'agent',
-                            'posts_per_page' => 10,
-                            'order' => 'ASC',
-                            'orderby' => 'menu_order',
-                            'post__in' => array($agent_id)
-                            );
+                    $wp_query = new WP_Query($args);
+                    while ( have_posts() ) : the_post();
+                    ?>
 
-                        $wp_query = new WP_Query($args);
-
-                        while ( have_posts() ) : the_post(); ?>
-
-                        <li class="accordion-group">
-                            <input type="radio" name="toggle-section-main-agent" id="toggle-target-<?php echo $wp_query->current_post; ?>"<?php if ( $wp_query->current_post == 0 ) echo ' checked'; ?>>
-                            <label for="toggle-target-<?php echo $wp_query->current_post; ?>" class="accordion-section-title">
-                                <span class="like-h3"><?php the_title(); ?></span>
-                                <?php if ( $area = shandora_get_meta( $post->ID, 'agentarea' ) ) { ?>
-                                <span class="right"><i class="icon bonicons bi-map-marker text main"></i> <?php echo $area; ?></span>
+                    <li class="accordion-group">
+                        <input type="radio" name="toggle-section-main-agent" id="toggle-target-<?php echo $wp_query->current_post; ?>"<?php if ( $wp_query->current_post == 0 ) echo ' checked'; ?>>
+                        <label for="toggle-target-<?php echo $wp_query->current_post; ?>" class="accordion-section-title">
+                            <span class="like-h3"><?php the_title(); ?></span>
+                            <?php if ( $area = shandora_get_meta( $post->ID, 'agentarea' ) ) { ?>
+                            <span class="right"><i class="icon bonicons bi-map-marker text main"></i> <?php echo $area; ?></span>
+                            <?php } ?>
+                        </label>
+                        <span class="accordion-open"><i class="sha-arrow-down"></i></span>
+                        <span class="accordion-close"><i class="sha-arrow-right"></i></span>
+                        <div class="toggle-content">
+                            <div class="agent-profile<?php echo has_post_thumbnail() ? ' has-img' : ''; ?>">
+                                <?php if ( has_post_thumbnail() ) { ?>
+                                <div class="profile-header center">
+                                    <?php if ( current_theme_supports( 'get-the-image' ) ) {
+                                        $thumbnail = get_the_image( array( 'size' => 'agent_small', 'link_to_post' => false, 'before' => '<figure class="agent-image">', 'after' => '</figure>' ) );
+                                    } ?>
+                                </div>
                                 <?php } ?>
-                            </label>
-                            <span class="accordion-open"><i class="sha-arrow-down"></i></span>
-                            <span class="accordion-close"><i class="sha-arrow-right"></i></span>
-                            <div class="toggle-content">
-                                <div class="agent-profile<?php echo has_post_thumbnail() ? ' has-img' : ''; ?>">
-                                    <?php if ( has_post_thumbnail() ) { ?>
-                                    <div class="profile-header center">
-                                        <?php if ( current_theme_supports( 'get-the-image' ) ) {
-                                            $thumbnail = get_the_image( array( 'size' => 'thumbnail', 'link_to_post' => false, 'before' => '<figure class="agent-image">', 'after' => '</figure>' ) );
-                                        } ?>
-                                    </div>
+                                <div class="profile-content">                        
+                                    <?php if ( $job = shandora_get_meta( $post->ID, 'agentjob' ) ) { ?>
+                                    <p class="like-h3"><?php echo $job; ?></p>
                                     <?php } ?>
-                                    <div class="profile-content">                        
-                                        <?php if ( $job = shandora_get_meta( $post->ID, 'agentjob' ) ) { ?>
-                                        <p class="like-h3"><?php echo $job; ?></p>
+                                    <ul class="bi-ul">
+                                        <?php if ( $area ) { ?>
+                                        <li><i class="bi-li icon bonicons bi-map-marker text main"></i> <?php echo $area; ?></li>
                                         <?php } ?>
-                                        <ul class="bi-ul">
-                                            <?php if ( $area ) { ?>
-                                            <li><i class="bi-li icon bonicons bi-map-marker text main"></i> <?php echo $area; ?></li>
-                                            <?php } ?>
-                                            <?php if ( $email = shandora_get_meta( $post->ID, 'agentemail' ) ) { ?>
-                                            <li><i class="bi-li icon bonicons bi-envelope text main"></i> <?php echo encrypt_email($email); ?></li>
-                                            <?php } ?>
-                                            <?php if ( $phone = shandora_get_meta( $post->ID, 'agentofficephone' ) ) { ?>
-                                            <li><i class="bi-li icon bonicons bi-phone text main"></i> <?php echo $phone; ?></li>
-                                            <?php } ?>
-                                            <?php if ( $mobile = shandora_get_meta( $post->ID, 'agentmobilephone' ) ) { ?>
-                                            <li><i class="bi-li icon bonicons bi-mobile text main"></i> <?php echo $mobile; ?></li>
-                                            <?php } ?>                                
-                                        </ul>
-                                    </div>
+                                        <?php if ( $email = shandora_get_meta( $post->ID, 'agentemail' ) ) { ?>
+                                        <li><i class="bi-li icon bonicons bi-envelope text main"></i> <?php echo encrypt_email($email); ?></li>
+                                        <?php } ?>
+                                        <?php if ( $phone = shandora_get_meta( $post->ID, 'agentofficephone' ) ) { ?>
+                                        <li><i class="bi-li icon bonicons bi-phone text main"></i> <?php echo $phone; ?></li>
+                                        <?php } ?>
+                                        <?php if ( $mobile = shandora_get_meta( $post->ID, 'agentmobilephone' ) ) { ?>
+                                        <li><i class="bi-li icon bonicons bi-mobile text main"></i> <?php echo $mobile; ?></li>
+                                        <?php } ?>                                
+                                    </ul>
                                 </div>
                             </div>
-                        </li>
+                        </div>
+                    </li>
 
-                        <?php endwhile;
+                    <?php
+                    endwhile;
+                    wp_reset_query();
+                    ?>
 
-                        wp_reset_query();
+                </ul>
+            </div>
+        </section>
+        <section class="row entry-content">
+            <header class="column large-12">
+                <h3><?php _e( 'Regional', 'bon' ); ?> <?php _e( 'representatives', 'bon' ); ?></h3>
+                <hr>
+            </header>
+            <div class="column large-12">
+                <ul class="bon-toolkit-accordion" id="accordion-agent">
 
-                        ?>
+                    <?php
+                    // setup loop to fetch 1 most expensive item for each category to display it's image as category image
+                    $args = array(
+                        'post_type' => 'agent',
+                        'posts_per_page' => 10,
+                        'order' => 'ASC',
+                        'orderby' => 'menu_order',
+                        'post__not_in' => array($agent_id)
+                        );
 
-                    </ul>
-                </div>
-            </section>
-            <section class="row entry-content">
-                <header class="column large-12">
-                    <h3><?php _e( 'Regional', 'bon' ); ?> <?php _e( 'representatives', 'bon' ); ?></h3>
-                    <hr>
-                </header>
-                <div class="column large-12">
-                    <ul class="bon-toolkit-accordion" id="accordion-agent">
+                    $wp_query = new WP_Query($args);
+                    while ( have_posts() ) : the_post();
+                    ?>
 
-                        <?php
-
-            // setup loop to fetch 1 most expensive item for each category to display it's image as category image
-                        $args = array(
-                            'post_type' => 'agent',
-                            'posts_per_page' => 10,
-                            'order' => 'ASC',
-                            'orderby' => 'menu_order',
-                            'post__not_in' => array($agent_id)
-                            );
-
-                        $wp_query = new WP_Query($args);
-
-                        while ( have_posts() ) : the_post(); ?>
-
-                        <li class="accordion-group">
-                            <input type="radio" name="toggle-section-agent" id="toggle-target-<?php echo $wp_query->current_post + 10; ?>"<?php if ( $wp_query->current_post == 0 ) echo ' checked'; ?>>
-                            <label for="toggle-target-<?php echo $wp_query->current_post + 10; ?>" class="accordion-section-title">
-                                <span class="like-h5"><?php the_title(); ?></span>
-                                <?php if ( $area = shandora_get_meta( $post->ID, 'agentarea' ) ) { ?>
-                                <span class="right"><i class="icon bonicons bi-map-marker text main"></i> <?php echo $area; ?></span>
+                    <li class="accordion-group">
+                        <input type="radio" name="toggle-section-agent" id="toggle-target-<?php echo $wp_query->current_post + 10; ?>"<?php if ( $wp_query->current_post == 0 ) echo ' checked'; ?>>
+                        <label for="toggle-target-<?php echo $wp_query->current_post + 10; ?>" class="accordion-section-title">
+                            <span class="like-h5"><?php the_title(); ?></span>
+                            <?php if ( $area = shandora_get_meta( $post->ID, 'agentarea' ) ) { ?>
+                            <span class="right"><i class="icon bonicons bi-map-marker text main"></i> <?php echo $area; ?></span>
+                            <?php } ?>
+                        </label>
+                        <span class="accordion-open"><i class="sha-arrow-down"></i></span>
+                        <span class="accordion-close"><i class="sha-arrow-right"></i></span>
+                        <div class="toggle-content">
+                            <div class="agent-profile<?php echo has_post_thumbnail() ? ' has-img' : ''; ?>">
+                                <?php if ( has_post_thumbnail() ) { ?>
+                                <div class="profile-header center">
+                                    <?php if ( current_theme_supports( 'get-the-image' ) ) {
+                                        $thumbnail = get_the_image( array( 'size' => 'thumbnail', 'link_to_post' => false, 'before' => '<figure class="agent-image">', 'after' => '</figure>' ) );
+                                    } ?>
+                                </div>
                                 <?php } ?>
-                            </label>
-                            <span class="accordion-open"><i class="sha-arrow-down"></i></span>
-                            <span class="accordion-close"><i class="sha-arrow-right"></i></span>
-                            <div class="toggle-content">
-                                <div class="agent-profile<?php echo has_post_thumbnail() ? ' has-img' : ''; ?>">
-                                    <?php if ( has_post_thumbnail() ) { ?>
-                                    <div class="profile-header center">
-                                        <?php if ( current_theme_supports( 'get-the-image' ) ) {
-                                            $thumbnail = get_the_image( array( 'size' => 'thumbnail', 'link_to_post' => false, 'before' => '<figure class="agent-image">', 'after' => '</figure>' ) );
-                                        } ?>
-                                    </div>
+                                <div class="profile-content">                        
+                                    <?php if ( $job = shandora_get_meta( $post->ID, 'agentjob' ) ) { ?>
+                                    <p class="like-h5"><?php echo $job; ?></p>
                                     <?php } ?>
-                                    <div class="profile-content">                        
-                                        <?php if ( $job = shandora_get_meta( $post->ID, 'agentjob' ) ) { ?>
-                                        <p class="like-h5"><?php echo $job; ?></p>
+                                    <ul class="bi-ul">
+                                        <?php if ( $area ) { ?>
+                                        <li><i class="bi-li icon bonicons bi-map-marker text main"></i> <?php echo $area; ?></li>
                                         <?php } ?>
-                                        <ul class="bi-ul">
-                                            <?php if ( $area ) { ?>
-                                            <li><i class="bi-li icon bonicons bi-map-marker text main"></i> <?php echo $area; ?></li>
-                                            <?php } ?>
-                                            <?php if ( $email = shandora_get_meta( $post->ID, 'agentemail' ) ) { ?>
-                                            <li><i class="bi-li icon bonicons bi-envelope text main"></i> <?php echo encrypt_email($email); ?></li>
-                                            <?php } ?>
-                                            <?php if ( $phone = shandora_get_meta( $post->ID, 'agentofficephone' ) ) { ?>
-                                            <li><i class="bi-li icon bonicons bi-phone text main"></i> <?php echo $phone; ?></li>
-                                            <?php } ?>
-                                            <?php if ( $mobile = shandora_get_meta( $post->ID, 'agentmobilephone' ) ) { ?>
-                                            <li><i class="bi-li icon bonicons bi-mobile text main"></i> <?php echo $mobile; ?></li>
-                                            <?php } ?>                                
-                                        </ul>
-                                    </div>
+                                        <?php if ( $email = shandora_get_meta( $post->ID, 'agentemail' ) ) { ?>
+                                        <li><i class="bi-li icon bonicons bi-envelope text main"></i> <?php echo encrypt_email($email); ?></li>
+                                        <?php } ?>
+                                        <?php if ( $phone = shandora_get_meta( $post->ID, 'agentofficephone' ) ) { ?>
+                                        <li><i class="bi-li icon bonicons bi-phone text main"></i> <?php echo $phone; ?></li>
+                                        <?php } ?>
+                                        <?php if ( $mobile = shandora_get_meta( $post->ID, 'agentmobilephone' ) ) { ?>
+                                        <li><i class="bi-li icon bonicons bi-mobile text main"></i> <?php echo $mobile; ?></li>
+                                        <?php } ?>                                
+                                    </ul>
                                 </div>
                             </div>
-                        </li>
+                        </div>
+                    </li>
 
-                        <?php endwhile;
+                    <?php
+                    endwhile;
+                    wp_reset_query();
+                    ?>
 
-                        wp_reset_query();
+                </ul>
+            </div>
+        </section>
 
-                        ?>
+        <?php if ( $headContact ) { ?>
 
-                    </ul>
-                </div>
-            </section>
+        <section class="row entry-content">
 
-            <?php if ( $headContact ) { ?>
+            <header class="column large-12">
+                <h3><?php echo $headContact; ?></h3>
+                <hr />
+            </header>
+            <div class="column large-12">
+                <?php echo $contentContact; ?>
+            </div>
+            <?php bon_get_template_part( 'block', 'contactform-faq' ); ?>
+        </section>
 
-            <section class="row entry-content">
+        <?php } ?>
 
-                <header class="column large-12">
-                    <h3><?php echo $headContact; ?></h3>
-                    <hr />
-                </header>
-                <div class="column large-12">
-                    <p><?php echo $contentContact; ?></p>
-                </div>
-                <?php bon_get_template_part( 'block', 'contactform-faq' ); ?>
-            </section>
+    </div>
 
-            <?php } ?>
-
-        </div>
-        
-        <?php 
+    <?php 
 
             /**
              * Shandora After Loop Hook
