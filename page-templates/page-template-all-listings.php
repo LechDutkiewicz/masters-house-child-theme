e11da616f5a61008233e56e56b4a05a7f8ff4af0
LechDutkiewicz <lechdutkiewicz@gmail.com>
2016-04-26 21:02:33 +0200

    update in all-listings page template

--- page-templates/page-template-all-listings.php
+++ page-templates/page-template-all-listings.php
@@ -22,26 +22,20 @@ get_header();
          *
          */
 
-        do_atomic('before_loop'); ?>
-
-        <?php
+        do_atomic('before_loop');
 
         // get categories that should be displayed on all categories page
         $cats = bon_get_option( 'cats_order' );
 
         foreach ($cats as $cat) {
 
-            // get term object for each category
+                // get term object for each category
             $term = get_term($cat['cat_name'], 'property-type');
             $id = $term->term_id;
-            // get color for each category
+                // get color for each category
             $color = get_option( "taxonomy_$id" );
 
-            ?>
-
-            <?php
-
-            // setup loop to fetch 1 most expensive item for each category to display it's image as category image
+                // setup loop to fetch 1 most expensive item for each category to display it's image as category image
             $listing_args = array(
                 'post_type' => 'listing',
                 'posts_per_page' => 1,
@@ -60,20 +54,12 @@ get_header();
 
             while ( have_posts() ) : the_post();
 
-            // bon_get_template_part( "content", "category" );
             include ( locate_template("templates/contents/content-category.php") );
 
             endwhile;
 
             wp_reset_query();
-
-            ?>
-
-            <?php
         }
-        ?>
-        
-        <?php 
 
             /**
              * Shandora After Loop Hook
@@ -84,9 +70,10 @@ get_header();
              *
              */
 
+            do_atomic('before_pagination');
+
             do_atomic('after_loop'); ?>
 
         </div>
-
 
         <?php get_footer(); ?>
